9017a1387c0b7ca0e210e9bc99116a0ab78b409f
Sydney McGee <mcgee.sydney@gmail.com>
2022-05-13 10:01:00 -0500

    start assignment problem

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from alarm_clock import AlarmClock
 
-clock_1 = AlarmClock()
+# clock_1 = AlarmClock()
 
-print(clock_1.alarm_time)
-clock_1.change_current_time("1000")
+# print(clock_1.alarm_time)
+# clock_1.change_current_time("1000")
 
-alarm_setting = clock_1.alarm_on_or_off(False)
-print(alarm_setting)
+# alarm_setting = clock_1.alarm_on_or_off(False)
+# print(alarm_setting)
+
---
+++ product.py
@@ -0,0 +1,7 @@
+
+class Product:
+
+    def __init__(self, name_of_product, product_price, product_category):
+        self.product_name = name_of_product
+        self.price = product_price
+        self.category = product_category
